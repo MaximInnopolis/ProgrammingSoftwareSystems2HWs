e9b9a0426bb4b4581cbb0459b4300af5edf43dc8
MaximInnopolis <m.latypov@innopolis.university>
2021-03-31 21:03:47 +0300

    New branch PSSHW4 created

---
+++ PSS2HW4/cmake-build-debug/CMakeFiles/PSS2HW4.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PSS2HW4.dir/main.cpp.obj"
+  "PSS2HW4.exe"
+  "PSS2HW4.exe.manifest"
+  "PSS2HW4.pdb"
+  "libPSS2HW4.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PSS2HW4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()

e1261d582d9327072ec56f276c2fd8cb31d5c794
MaximInnopolis <m.latypov@innopolis.university>
2021-04-19 20:40:17 +0300

    Project created

---
+++ PSS2HW5/cmake-build-debug/CMakeFiles/PSS2HW5.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PSS2HW5.dir/main.cpp.obj"
+  "PSS2HW5.exe"
+  "PSS2HW5.exe.manifest"
+  "PSS2HW5.pdb"
+  "libPSS2HW5.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/PSS2HW5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ PSS2HW5/cmake-build-debug/CMakeFiles/PSS2HW5.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for PSS2HW5.
+# This may be replaced when dependencies are built.
